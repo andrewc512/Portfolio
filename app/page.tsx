8d4407e209b9802c87f9051fbd3aa28f746b9d57
andrewc512 <134175823+andrewc512@users.noreply.github.com>
2024-09-13 21:28:25 +0800

    first commit without animated hero

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Hero from "@/components/Hero";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <div>
+        <Hero/>
+      </div>
+    </main>
+  );
+}
---
+++ components/Hero.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const Hero = () => {
+    return (
+        <section className="flex items-center justify-center h-screen bg-gray-950 text-white px-6">
+            <div className='max-w-4xl text-left'>
+                <h1 className='text-5xl md:text-6xl lg:text-6xl font-serif mb-8'>I am Andrew</h1>
+                <h1 className="text-5xl md:text-6xl lg:text-6xl font-serif mb-12">I am a software developer
+
+                </h1>
+            </div>
+        </section>
+    )
+}
+
+export default Hero
