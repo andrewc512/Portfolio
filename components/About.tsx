0a2bb48cb325fbe288224d49ed1079423a2834e2
andrewc512 <134175823+andrewc512@users.noreply.github.com>
2024-09-13 22:58:09 +0800

    added framer motion I think

---
+++ components/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <section className='h-screen' id="about">
+            <div>About</div>
+        </section>
+
+    )
+}
+
+export default About
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,20 +6,23 @@ import { ReactTyped } from 'react-typed'
 
 const Hero = () => {
     return (
-        <section className="flex items-center justify-left h-screen bg-gray-950 text-white px-6">
+        <section className="flex items-center justify-left h-screen bg-gray-950 text-white px-6" id="home">
             <div className='max-w-4xl text-left px-20'>
                 <h1 className='text-4xl md:text-6xl lg:text-6xl font-serif mb-8'>I am Andrew</h1>
                 <h1 className="text-4xl md:text-6xl lg:text-6xl font-serif mb-12">I am a {""}
                     <ReactTyped
                         strings={["Software Engineer", "College Student", "Software Developer"]}
-                        typeSpeed={100}
+                        typeSpeed={80}
                         backSpeed={80}
                         loop
                     />
                 </h1>
-                <button className="shadow-[inset_0_0_0_2px_#616467] text-black px-12 py-4 rounded-full tracking-widest uppercase font-bold bg-transparent hover:bg-[#616467] hover:text-white dark:text-neutral-200 transition duration-200">
-                    Lets Explore
-                </button>
+                <a href='#about'>
+                    <button className="shadow-[inset_0_0_0_2px_#616467] text-black px-12 py-4 rounded-full tracking-widest uppercase font-bold bg-transparent hover:bg-[#616467] hover:text-white dark:text-neutral-200 transition duration-200">
+                        Lets Explore
+                    </button>
+                </a>
+
             </div>
         </section>
     )
