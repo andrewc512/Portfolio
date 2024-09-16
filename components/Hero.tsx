a2ae7e8a1a58ea8e77d746d5426842c79e4be0a6
andrewc512 <134175823+andrewc512@users.noreply.github.com>
2024-09-16 12:37:45 +0800

    fixed hero button

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -18,8 +18,8 @@ const Hero = () => {
                     />
                 </h1>
                 <a href='#about'>
-                    <button className="shadow-[inset_0_0_0_2px_#616467] text-black px-12 py-4 rounded-full tracking-widest uppercase font-bold bg-transparent hover:bg-[#616467] hover:text-white dark:text-neutral-200 transition duration-200">
-                        Lets Explore
+                    <button className="shadow-[inset_0_0_0_2px_#616467] px-12 py-4 rounded-full tracking-widest uppercase font-bold bg-transparent hover:bg-[#616467] hover:text-white text-neutral-200 transition duration-200">
+                        Learn More
                     </button>
                 </a>
 
